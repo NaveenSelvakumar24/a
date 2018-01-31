67dfc3f498435043b4dedf62246f5c4f4f4a7065
NaveenSelvakumar24 <30549885+NaveenSelvakumar24@users.noreply.github.com>
2018-01-31 15:50:20 +0530

    Create 24.c

---
+++ 24.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+{
+int l, r;
+quickSort(A, 0, arr_size-1);
+array*/= 0;
+r = arr_size-1; 
+while (l < r)
+{
+if(A[l] + A[r] == sum)
+return 1; 
+else if(A[l] + A[r] < sum)
+l++;
+else 
+r--;
+}    
+return 0;
+}
+ 
