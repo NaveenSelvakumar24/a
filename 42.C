3554dbe0d60465dd5bfbdd50d98614aafd76b8af
NaveenSelvakumar24 <30549885+NaveenSelvakumar24@users.noreply.github.com>
2018-02-19 16:35:46 +0530

    Create 42.C

---
+++ 42.C
@@ -0,0 +1,22 @@
+#include<stdio.h>
+include<conio.h>
+void main()
+{
+int i;
+char s1[10],s2[10];
+gets(s1);
+gets(s2);
+if(s1[i]>s2[i])
+{
+printf("%s is greater",s1);
+}
+else if(s1[i]<s2[i])
+{
+printf("%s is greater"s2);
+}
+else
+{
+printf("both are equal");
+}
+getch();
+}
