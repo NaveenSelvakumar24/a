707349a7e6cb2e36bec08b2106bb90b3cd5aa2ab
NaveenSelvakumar24 <30549885+NaveenSelvakumar24@users.noreply.github.com>
2018-02-21 15:42:11 +0530

    Create 44.C

---
+++ 44.C
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int num;
+printf("Enter a number");
+scanf("%d",&num);
+if(num<=10)
+{
+printf("The number is less than 10");
+}
+else
+{
+printf("The number is greater than 10");
+}
+getch();
+}
