8a5b21768d85858b8aaa6150607c50590d12ea3d
NaveenSelvakumar24 <30549885+NaveenSelvakumar24@users.noreply.github.com>
2018-02-23 15:56:37 +0530

    Create 52.C

---
+++ 52.C
@@ -0,0 +1,32 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int num;
+printf("Enter the number from one to ten");
+scanf("%d",&num);
+switch(num)
+{
+case 1:
+printf("\n one");
+break;
+case 2:
+printf("\n two");
+break;
+case 3:
+printf("\n three");
+break;
+case 4:
+printf("\n four");
+break;
+case 5:
+printf("\n five");
+break;
+case 6:
+printf("\n six");
+break;
+case 7:
+printf("\n seven");
+break;
+case 8:
+printf("\n 
