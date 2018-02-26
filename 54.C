41053e2eb1d4615849989e6dba5e3b1d4b055834
NaveenSelvakumar24 <30549885+NaveenSelvakumar24@users.noreply.github.com>
2018-02-26 15:39:33 +0530

    Update 54.C

--- 54.C
+++ 54.C
@@ -2,4 +2,17 @@
 #include<conio.h>
 void main()
 {
-int
+int num,even;
+printf("enter the number:");
+scanf("%d",&num);
+if(num%2==0)
+{
+even=num-2;	
+}
+else
+{
+even=num-1;
+}
+printf("\n%d",even);
+getch();
+}
