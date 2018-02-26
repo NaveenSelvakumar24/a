ac43a3c7fb7515dd5583b69bdb1b47d387cacdf2
NaveenSelvakumar24 <30549885+NaveenSelvakumar24@users.noreply.github.com>
2018-02-26 16:09:38 +0530

    Create 57.C

---
+++ 57.C
@@ -0,0 +1,18 @@
+#include<stdio.h>
+void swap(int a,int b);
+void main()
+{
+int *x,*y;
+x=10;
+y=5;
+printf("%d %d",x,y);
+}
+void swap(int a,int b)
+{
+*a,*b;
+a=10;
+b=5;
+printf("%d %d",a,b);
+return 0;
+}
+
