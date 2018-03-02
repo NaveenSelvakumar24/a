aa98a103c18c0311f09491d1db7c43eb85981b49
NaveenSelvakumar24 <30549885+NaveenSelvakumar24@users.noreply.github.com>
2018-03-02 15:34:18 +0530

    Update 58.c

--- 58.c
+++ 58.c
@@ -6,6 +6,6 @@ void main()
  printf("enter the string:");
  scanf("%[^\n]s",&s1);
  cha=strchr(s1,ch);
- printf("String after %c is : %s",ch ,cha+1);
+ printf("String after %c is : %s",ch,cha+1);
  getch();
 }
