a5b4cc9953f14265ed44d4468b25bce190429780
NaveenSelvakumar24 <30549885+NaveenSelvakumar24@users.noreply.github.com>
2018-03-02 16:30:52 +0530

    Create 62.C

---
+++ 62.C
@@ -0,0 +1,30 @@
+#include<stdio.h>
+#include<conio.h>
+#include<string.h>
+void main()
+{
+char a[10];
+scanf("%s",&a);
+int i,flag=0,m;
+m=strlen;
+for(i=0;i<=m;i++)
+{
+if(if(a[i]=='1' || a[i]=='0')
+{
+flag=1;
+}
+else
+{
+flag=0;
+}
+if(flag==1)
+{
+printf("Yes");
+}
+else
+{
+printf("no");
+}
+}
+getch();
+}
