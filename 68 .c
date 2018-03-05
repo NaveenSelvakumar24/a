579473cb27841103da0756a26c6b10b2adbe435d
NaveenSelvakumar24 <30549885+NaveenSelvakumar24@users.noreply.github.com>
2018-03-05 16:25:08 +0530

    Create 68 .c

---
+++ 68 .c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int n;
+printf("Enter a number");
+scanf("%d",&n);
+if(n%7==0)
+{
+printf("The number is multiple of 7");
+}
+else
+{
+printf("The number is not a multiple of 7");
+}
+getch();
+}
