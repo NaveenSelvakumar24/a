0c9e58ecc3fca8e4a8ea39b0a9d800370905e6e9
NaveenSelvakumar24 <30549885+NaveenSelvakumar24@users.noreply.github.com>
2018-03-14 15:48:01 +0530

    Update 83.C

--- 83.C
+++ 83.C
@@ -2,6 +2,20 @@
 #include<conio.h>
 void main()
 {
-int maths,science;
-printf("Enter the marks");
-scanf("
+int n1,n2,i;
+char sym;
+printf("\n enter the two numbers:\n");
+for(i=1;i<=4;i++)
+{
+scanf("%d %c %d",&n1,&sym,&n2);
+if(sym=='/')
+{
+printf("%d\n",n1/n2);
+}
+else
+{
+printf("%d\n",n1%n2);
+}
+getch();
+}
+  
