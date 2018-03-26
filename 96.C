b4e1d1055df03f320f2e6914a4a6cab7132ff7b1
NaveenSelvakumar24 <30549885+NaveenSelvakumar24@users.noreply.github.com>
2018-03-26 15:56:53 +0530

    Update 96.C

--- 96.C
+++ 96.C
@@ -5,3 +5,24 @@ void main()
 int n;
 printf("Enter a number");
 scanf("%d",&n);
+if(n%2!=0 && n>3)
+{
+for(i=3;i<n;i++)
+{
+if(n%i==0)
+{
+flag=1;
+break;
+}
+}
+}
+if(flag==1)
+{
+printf("composite number");
+}
+else
+{
+printf("not a composite number");
+}
+getch();
+}
