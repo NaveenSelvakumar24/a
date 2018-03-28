32c566a58ed54318385c27695fb0a9365e564bc5
NaveenSelvakumar24 <30549885+NaveenSelvakumar24@users.noreply.github.com>
2018-03-28 16:16:18 +0530

    Rename 100.C to 1000.C

---
+++ 1000.C
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int n,remainder,product=1;
+printf("Enter a number");
+scanf("%d",&n);
+while(n!=0)
+{
+remainder=n%10;
+product=product*remainder;
+n=n/10;
+}
+printf("\n%d",product);
+getch();
+}
