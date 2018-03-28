c8be16d8d7b2b542e6532f15a45241a3af47bb16
NaveenSelvakumar24 <30549885+NaveenSelvakumar24@users.noreply.github.com>
2018-03-28 16:35:22 +0530

    Create 104.C

---
+++ 104.C
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include<conio.h>
+int main(void) 
+{
+int n,k,i,result=1;
+printf("Enter n and k");
+scanf("%d %d",&n,&k);
+for(i=1;i<=k;i++)
+{
+result=result*n;	
+}
+printf("%d",result);
+getch();
+}
