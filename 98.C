9b7eacc4e3f8a9584b50376615f3237a8a83a2ad
NaveenSelvakumar24 <30549885+NaveenSelvakumar24@users.noreply.github.com>
2018-03-28 15:51:45 +0530

    Create 98.C

---
+++ 98.C
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#inclde<conio.h>
+void main()
+{
+int a[3],i,j,max;
+for(i=1;i<=4;i++)
+scanf("%d",&a[i]);
+for(i=1;i<=4;i++)
+{
+for(j=i+1;j<=4;j++)
+{
+if(a[i]>a[j])
+{
+max=a[i];
+printf("%d\n",i);
+}
+}
+}
+getch();
+}
